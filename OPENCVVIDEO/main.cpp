eaf86031064cc3bbcfc8b918df1d9f1a5d495862
Rahsaan Bleijs <rahsaan@live.nl>
2022-03-22 19:29:05 +0100

    CVVC

--- OPENCVVIDEO/main.cpp
+++ OPENCVVIDEO/main.cpp
@@ -39,6 +39,36 @@ while(true){
         }
       }
 
+            Mat canny_output;
+            vector<vector<Point> > contours;
+            vector<Vec4i> hierarchy;
+
+            Canny(gray_camera, canny_output, 100, 500,3);
+
+            findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+
+            vector<Moments> mu(contours.size());
+            for( int i = 0; i<contours.size(); i++ )
+            { mu[i] = moments( contours[i], false ); }
+
+            // get the centroid of figures.
+            vector<Point2f> mc(contours.size());
+            for( int i = 0; i<contours.size(); i++)
+            { mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); }
+
+            Mat drawing(canny_output.size(), CV_8UC3, Scalar(255,255,255));
+            for( int i = 0; i<contours.size(); i++ )
+            {
+            Scalar color = Scalar(167,151,111); // B G R values
+            drawContours(camera, contours, i, color, 2, 8, hierarchy, 0, Point());
+            circle( camera, mc[i], 5, Scalar(0,0,0), -1, 8, 0);
+            std::cout << mc[i] << std::endl;
+            }
+
+
+
+
       Moments m = moments(gray_camera,true);
       Point p(m.m10/m.m00, m.m01/m.m00);
       cout<< Mat(p)<< endl;
