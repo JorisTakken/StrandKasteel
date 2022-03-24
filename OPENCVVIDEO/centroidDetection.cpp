65404bffea6a5401dd0f2d362a006dee1837e65c
Rahsaan Bleijs <rahsaan@live.nl>
2022-03-24 12:10:36 +0100

    groen

--- OPENCVVIDEO/centroidDetection.cpp
+++ OPENCVVIDEO/centroidDetection.cpp
@@ -15,7 +15,7 @@ void CentroidDetection::binaryImage(Mat& camera){
     cvtColor(camera, gray_camera, COLOR_BGR2GRAY);
     int h = gray_camera.rows;
     int w = gray_camera.cols;
-    cout << h<< endl;
+    // cout << h<< endl;
 
 
     for(int i = 0; i < h; i++) {
@@ -33,27 +33,27 @@ void CentroidDetection::edgeDetection(){
 
 void CentroidDetection::findingcontours(){
     findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-    cout << contours.size() << endl;
+    // cout << contours.size() << endl;
 
-    
-}   
+
+}
 
 void CentroidDetection::makePoints(){
-    // make points 
+    // make points
 
     // std::vector<int> arr = std::vector<int> (20);
     mu = vector<Moments>(contours.size());
     mc = vector<Point2f>(contours.size());
 
     // mu(contours.size());
-    // mc(contours.size()); 
+    // mc(contours.size());
 
-    for( int i = 0; i<contours.size(); i++ ){ 
-      mu[i] = moments(contours[i], false ); 
+    for( int i = 0; i<contours.size(); i++ ){
+      mu[i] = moments(contours[i], false );
     }
     // get the centroid of figures with formula
-    for( int i = 0; i<contours.size(); i++){ 
-      mc[i] = Point2f(mu[i].m10/(mu[i].m00 + 1e-5 ), mu[i].m01/(mu[i].m00 + 1e-5)); 
+    for( int i = 0; i<contours.size(); i++){
+      mc[i] = Point2f(mu[i].m10/(mu[i].m00 + 1e-5 ), mu[i].m01/(mu[i].m00 + 1e-5));
     }
 }
 
@@ -67,7 +67,7 @@ void CentroidDetection::drawPoints(Mat& camera){
     for( int i = 0; i<contours.size(); i++ ){
       Scalar color = Scalar(167,151,111); // B G R values
       drawContours(camera, contours, i, color, 2, 8, hierarchy, 0, Point());
-      circle(camera, mc[i], 5, Scalar(0,0,0), -1, 8, 0);
-      std::cout << mc[i] << std::endl;
+      circle(camera, mc[i], 5, Scalar(0,255,0), -1, 8, 0);
+      // std::cout << mc[i] << std::endl;
     }
 }
