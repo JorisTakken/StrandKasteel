5b8248d0fec5a6ae57ff87af68986cdff0856969
Rahsaan Bleijs <rahsaan@live.nl>
2022-03-24 14:28:02 +0100

    added parsing x/y for centroids

--- OPENCVVIDEO/centroidDetection.cpp
+++ OPENCVVIDEO/centroidDetection.cpp
@@ -15,8 +15,6 @@ void CentroidDetection::binaryImage(Mat& camera){
     cvtColor(camera, gray_camera, COLOR_BGR2GRAY);
     int h = gray_camera.rows;
     int w = gray_camera.cols;
-    cout << h<< endl;
-
 
     for(int i = 0; i < h; i++) {
         uchar* row = gray_camera.ptr<uchar>(i);
@@ -33,36 +31,34 @@ void CentroidDetection::edgeDetection(){
 
 void CentroidDetection::findingcontours(){
     findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-    cout << contours.size() << endl;
 
-    
-}   
+
+}
 
 void CentroidDetection::makePoints(){
-    // make points 
+    // make points
 
     // std::vector<int> arr = std::vector<int> (20);
     mu = vector<Moments>(contours.size());
     mc = vector<Point2f>(contours.size());
 
     // mu(contours.size());
-    // mc(contours.size()); 
+    // mc(contours.size());
 
-    for( int i = 0; i<contours.size(); i++ ){ 
-      mu[i] = moments(contours[i], false ); 
+    for( int i = 0; i<contours.size(); i++ ){
+      mu[i] = moments(contours[i], false );
     }
     // get the centroid of figures with formula
-    for( int i = 0; i<contours.size(); i++){ 
-      mc[i] = Point2f(mu[i].m10/(mu[i].m00 + 1e-5 ), mu[i].m01/(mu[i].m00 + 1e-5)); 
+    for( int i = 0; i<contours.size(); i++){
+      mc[i] = Point2f(mu[i].m10/(mu[i].m00 + 1e-5 ), mu[i].m01/(mu[i].m00 + 1e-5));
     }
 }
 
 void CentroidDetection::drawBigCentroid(Mat& camera){
     Moments m = moments(gray_camera,true);
     Point p(m.m10/m.m00, m.m01/m.m00);
-    cout<< Mat(p)<< endl;
     // circle(camera,(447,63), 63, (0,0,255), -1);
-    circle(camera, p, 50, Scalar(255,0,0), -1);
+    circle(camera, p, 5, Scalar(255,0,0), -1);
 }
 
 void CentroidDetection::drawPoints(Mat& camera){
@@ -75,9 +71,19 @@ void CentroidDetection::drawPoints(Mat& camera){
     for( int i = 0; i<contours.size(); i++ ){
       Scalar color = Scalar(167,151,111); // B G R values
       drawContours(camera, contours, i, color, 2, 8, hierarchy, 0, Point());
-      circle(camera, mc[i], 5, Scalar(0,0,0), -1, 8, 0);
-      std::cout << mc[i] << std::endl;
+      circle(camera, mc[i], 5, Scalar(0,255,0), -1, 8, 0);
     }
 }
 
+void CentroidDetection::listGen(){
+  for(int i = 0; i < mc.size();i++){
+    if(mc[i] != Point2f(0)){
+      xValues = vector<float>(mc.size());
+      yValues = vector<float>(mc.size());
+      xValues[i] = float(mc[i].x);
+      yValues[i] = float(mc[i].y);
+    }
 
+
+  }
+}
